90e2f26716d5aab8efc2cdfb132004a6c94e93e0
张俊毅 <wb-zhangjunyi@taobao.com>
2012-02-27 21:13:39 +0800

    加入一些测试结果链接

--- index.php
+++ index.php
@@ -15,7 +15,13 @@
     </ol>
     <h2>W3C Test</h2>
     <ol>
-    <li><a href="http://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/index.html">CSS3 Selectors Test Suite Index</a></li>
+        <li><a href="http://www.w3.org/Style/CSS/Test/CSS3/Selectors/current/index.html">CSS3 Selectors Test Suite Index</a></li>
+    </ol>
+    <h2>Other Test</h2>
+    <ol>
+        <li><a href="http://labs.qianduan.net/css-selector/" target="_blank">CSS选择器的浏览器支持</a></li>
+        <li><a href="http://tools.css3.info/selectors-test/test.html" target="_blank">CSS3 Selectors Test</a></li>
+        <li><a href="http://kb.cnblogs.com/a/925900/" target="_blank">CSS selectors: basic browser support</a></li>
     </ol>
 </body>
 </html>
