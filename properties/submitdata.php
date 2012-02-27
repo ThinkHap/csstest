af1a8c819eb90b18aa508623f62959885b842e12
王浩 <wb-wanghao@taobao.com>
2012-02-27 18:13:17 +0800

    css properties test

---
+++ properties/submitdata.php
@@ -0,0 +1,21 @@
+<?php
+    $con = mysql_connect("http://ued.etao.net","csstest","csstest");
+    if (!$con) {
+        die('Could not connect: ' . mysql_error());
+    };
+    
+    $t = time();
+    $time = date("Y-m-d H:i:s", $t);
+
+    mysql_select_db("test", $con);
+    
+    $sql="INSERT INTO css (time, browser, uastring, cssresult) VALUES ('$time', '$_POST[browser]', '$_POST[uastring]', '$_POST[cssresult]')";
+    
+    if (!mysql_query($sql,$con)) {
+        die('Error: ' . mysql_error());
+    };
+
+    //echo "1 record added";
+    
+    mysql_close($con)
+?>
