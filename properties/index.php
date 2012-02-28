723b1f3ab7097fc1497337a71dd9518be0320742
王浩 <wb-wanghao@taobao.com>
2012-02-28 22:16:12 +0800

    重建mysql数据表

--- properties/index.php
+++ properties/index.php
@@ -1,3 +1,362 @@
+<?php
+if (array_key_exists('browser', $_POST)) {
+    $con = mysql_connect("127.0.0.1","root","wanghao");
+    if (!$con) {
+        die('Could not connect: ' . mysql_error());
+    };
+        
+    $properties = array(
+        'background',
+        'background-attachment',
+        'background-clip',
+        'background-color',
+        'background-image',
+        'background-origin',
+        'background-position',
+        'background-position-x',
+        'background-position-y',
+        'background-repeat',
+        'background-repeat-x',
+        'background-repeat-y',
+        'border',
+        'border-bottom',
+        'border-bottom-color',
+        'border-bottom-left-radius',
+        'border-bottom-right-radius',
+        'border-bottom-style',
+        'border-bottom-width',
+        'border-color',
+        'border-image',
+        'border-image-outset',
+        'border-image-repeat',
+        'border-image-slice',
+        'border-image-source',
+        'border-image-width',
+        'border-left',
+        'border-left-color',
+        'border-left-style',
+        'border-left-width',
+        'border-radius',
+        'border-right',
+        'border-right-color',
+        'border-right-style',
+        'border-right-width',
+        'border-style',
+        'border-top',
+        'border-top-color',
+        'border-top-left-radius',
+        'border-top-right-radius',
+        'border-top-style',
+        'border-top-width',
+        'border-length',
+        'border-width',
+        'position',
+        'z-index',
+        'top',
+        'right',
+        'bottom',
+        'left',
+        'color-interpolation',
+        'color-interpolation-filters',
+        'color-profile',
+        'color-rendering',
+        'opacity',
+        'content',
+        'counter-increment',
+        'counter-reset',
+        'quotes',
+        'font',
+        'font-effect',
+        'font-emphasize',
+        'font-emphasize-position',
+        'font-emphasize-style',
+        'font-family',
+        'font-size',
+        'font-stretch',
+        'font-style',
+        'font-variant',
+        'font-weight',
+        'lighting-color',
+        'list-style',
+        'list-style-image',
+        'list-style-position',
+        'list-style-type',
+        'display',
+        'float',
+        'float-offset',
+        'clear',
+        'visibility',
+        'clip',
+        'clip-path',
+        'clip-rule',
+        'overflow',
+        'overflow-style',
+        'overflow-x',
+        'overflow-y',
+        'max-width',
+        'min-width',
+        'max-height',
+        'min-height',
+        'margin',
+        'margin-bottom',
+        'margin-left',
+        'margin-right',
+        'margin-top',
+        'outline',
+        'outline-color',
+        'outline-offset',
+        'outline-style',
+        'outline-width',
+        'nav-index',
+        'nav-up',
+        'nav-right',
+        'nav-down',
+        'nav-left',
+        'cursor',
+        'zoom',
+        'box-align',
+        'box-direction',
+        'box-flex',
+        'box-flex-groups',
+        'box-lines',
+        'box-ordinal-groups',
+        'box-orient',
+        'box-pack',
+        'box-reflect',
+        'box-sizing',
+        'box-shadow',
+        'resize',
+        'padding',
+        'padding-bottom',
+        'padding-left',
+        'padding-right',
+        'padding-top',
+        'page',
+        'page-break-after',
+        'page-break-before',
+        'page-break-inside',
+        'page-policy',
+        'border-collapse',
+        'table-layout',
+        'border-spacing',
+        'caption-side',
+        'empty-cells',
+        'direction',
+        'tab-size',
+        'text-align',
+        'text-align-last',
+        'text-anchor',
+        'text-decoration',
+        'text-emphasis',
+        'text-height',
+        'text-indent',
+        'text-justify',
+        'text-outline',
+        'text-replace',
+        'text-line-through',
+        'text-line-through-color',
+        'text-line-through-mode',
+        'text-line-through-style',
+        'text-line-through-width',
+        'text-overflow',
+        'text-overline',
+        'text-overline-color',
+        'text-overline-mode',
+        'text-overline-style',
+        'text-overline-width',
+        'text-rendering',
+        'text-shadow',
+        'text-transform',
+        'text-underline',
+        'text-underline-color',
+        'text-underline-mode',
+        'text-underline-style',
+        'text-underline-width',
+        'text-wrap',
+        'letter-spacing',
+        'vertical-align',
+        'unicode-bidi',
+        'line-height',
+        'line-stacking',
+        'line-stacking-ruby',
+        'line-stacking-shift',
+        'line-stacking-strategy',
+        'width',
+        'height',
+        'device-width',
+        'device-height',
+        'orientation',
+        'aspect-ratio',
+        'device-aspect-ratio',
+        'color',
+        'color-index',
+        'monochrome',
+        'resolution',
+        'scan',
+        'grid',
+        'grid-columns',
+        'grid-rows',
+        'alignment-adjust',
+        'alignment-baseline',
+        'appearance',
+        'baseline-shift',
+        'binding',
+        'bookmark-label',
+        'bookmark-level',
+        'bookmark-target',
+        'column-axis',
+        'column-break-after',
+        'column-break-before',
+        'column-break-inside',
+        'column-count',
+        'column-fill',
+        'column-gap',
+        'column-rule',
+        'column-rule-color',
+        'column-rule-style',
+        'column-rule-width',
+        'column-span',
+        'column-width',
+        'columns',
+        'crop',
+        'cue',
+        'cue-after',
+        'cue-before',
+        'dominant-baseline',
+        'drop-initial-after-adjust',
+        'drop-initial-after-align',
+        'drop-initial-before-adjust',
+        'drop-initial-before-align',
+        'drop-initial-size',
+        'drop-initial-value',
+        'enable-background',
+        'elevation',
+        'fill',
+        'fill-opacity',
+        'fill-rule',
+        'filter',
+        'fit',
+        'fit-position',
+        'flood-color',
+        'flood-opacity',
+        'glyph-orientation-horizontal',
+        'glyph-orientation-vertical',
+        'hanging-punctuations',
+        'hyphenate-after',
+        'hyphenate-before',
+        'hyphenate-character',
+        'hyphenate-lines',
+        'hyphenate-resource',
+        'hyphens',
+        'icon',
+        'image-rendering',
+        'image-orientation',
+        'image-resolution',
+        'inline-box-align',
+        'mark',
+        'mark-after',
+        'mark-before',
+        'mark-offset',
+        'marks',
+        'marker',
+        'marker-mid',
+        'marker-end',
+        'marker-start',
+        'marquee-direction',
+        'marquee-play-count',
+        'marquee-speed',
+        'marquee-style',
+        'mask',
+        'move-to',
+        'orphans',
+        'pointer-events',
+        'pause',
+        'pause-after',
+        'pause-before',
+        'phonemes',
+        'pitch',
+        'pitch-range',
+        'play-during',
+        'presentation-level',
+        'punctuation-trim',
+        'richness',
+        'rendering-indent',
+        'rest',
+        'rest-after',
+        'rest-before',
+        'rotation',
+        'rotation-point',
+        'ruby-align',
+        'ruby-overhang',
+        'ruby-position',
+        'ruby-span',
+        'shape-rendering',
+        'size',
+        'speak',
+        'speak-header',
+        'speak-numeral',
+        'speak-punctuation',
+        'speech-rate',
+        'stress',
+        'string-set',
+        'src',
+        'stop-color',
+        'stop-opacity',
+        'stroke',
+        'stroke-dasharray',
+        'stroke-dashoffset',
+        'stroke-linecap',
+        'stroke-linejoin',
+        'stroke-miterlimit',
+        'stroke-opacity',
+        'stroke-width',
+        'target',
+        'target-name',
+        'target-new',
+        'target-position',
+        'unicode-range',
+        'vertor-effect',
+        'voice-balance',
+        'voice-duration',
+        'voice-family',
+        'voice-pitch',
+        'voice-pitch-range',
+        'voice-rate',
+        'voice-stress',
+        'voice-volume',
+        'volume',
+        'white-space',
+        'white-space-collapse',
+        'windows',
+        'word-break',
+        'word-spacing',
+        'word-wrap',
+        'writing-mode');
+
+    sort($properties);
+    $prop = join("`, `", $properties);
+
+    $results = array();
+    for ($i = 0; $i < count($properties); $i++) {
+        $post = $_POST[$properties[$i]];
+        array_push($results, $post);
+    }
+    $res = join("', '", $results);
+
+    $t = time();
+//    $time = date("Y-m-d H:i:s", $t);
+
+    mysql_select_db("test", $con);
+    
+    $sql="INSERT INTO newcss (`browser`, `uastring`, `$prop`) VALUES ('$_POST[browser]', '$_POST[uastring]', '$res')";
+    
+    if (!mysql_query($sql,$con)) {
+        die('Error: ' . mysql_error());
+    };
+    mysql_close($con);
+    die;
+}
+?>
 <!doctype html>
 <html>
     <head>
@@ -34,6 +393,7 @@
         </div>
     </body>
         <script>
+/*
             var properties = [
                 'background',
                 'background-attachment',
@@ -366,6 +726,11 @@
                 'word-wrap',
                 'writing-mode'
             ];
+*/
+<?php
+$p = join(", ", $properties);
+echo 'var properties = ['. $p .']';
+?>
             cssProperties = properties.sort();
             
             var result = [];
@@ -399,30 +764,21 @@
                     getStyle = getStyleProperty(_this);
                 
                 // 0. origin results
-                //if (getStyle == true){
-                //    result.push('<td><span class="css-property">' + _this + '</span></td>');
-                //}else if (getStyle) {
-                //    result.push('<td><span class="css-property">' + _this + '</span></td>');
-                //}else {
-                //    result.push('<td><span class="css-property">' + _this + '</span></td>');
-                //};
-
-                // 0. origin results
                 if (getStyle == true){
-                    result.push('<td><span class="support">Y</span></td>');
                     fullresult.push('<tr><td><span class="css-property">' + _this + '</span></td>' + '<td><span class="support">Y</span></td></tr>');
+                    result.push(_this +'=Y');
                 }else if (getStyle) {
-                    result.push('<td><span class="support">' + getStyle + '</span></td>');
+                    result.push(_this +'='+ getStyle);
                     fullresult.push('<tr><td><span class="css-property">' + _this + '</span></td>' + '<td><span class="support">' + getStyle + '</span></td></tr>');
                 }else {
-                    result.push('<td><span class="unsupport">N</span></td>');
+                    result.push(_this +'=N');
                     fullresult.push('<tr><td><span class="css-property">' + _this + '</span></td>' + '<td><span class="unsupport">N</span></td></tr>');
                 };
 
             
             };       
 
-            var cssresult = result.join("\r");
+            var cssresult = result.join("&");
             var time = new Date();
             var ua = navigator.userAgent;
             //var ua = KISSY.UA;
@@ -433,27 +789,6 @@
             document.write(resultHtml);
 
 
-            //if(navigator.userAgent){
-            //    var ua = navigator.userAgent.toLowerCase();  //ªÒ»°”√ªß∂À–≈œ¢
-            //    var info = {
-            //        ie: /msie/.test(ua) && !/opera/.test(ua),    //∆•≈‰IE‰Ø¿¿∆˜
-            //        op: /opera/.test(ua),   //∆•≈‰Opera‰Ø¿¿∆˜
-            //        sa: /version.*safari/.test(ua) && !/android/.test(ua),   //∆•≈‰Safari‰Ø¿¿∆˜
-            //        ch: /chrome/.test(ua),   //∆•≈‰Chrome‰Ø¿¿∆˜
-            //        ff: /firefox/.test(ua) && !/webkit/.test(ua),   //∆•≈‰Firefox‰Ø¿¿∆˜
-            //        an: /android/.test(ua),   //∆•≈‰Firefox‰Ø¿¿∆˜
-            //        ip: /iPhone.*OS/.test(ua)   //∆•≈‰Firefox‰Ø¿¿∆˜
-            //    };
-
-            //    var browser;
-            //    if(info.ie) browser = "IE‰Ø¿¿∆˜";
-            //    if(info.op) browser = "Opera‰Ø¿¿∆˜";
-            //    if(info.sa) browser = "Safari‰Ø¿¿∆˜";
-            //    if(info.ff) browser = "Firefox‰Ø¿¿∆˜";
-            //    if(info.ch) browser = "Chrome‰Ø¿¿∆˜";
-            //    if(info.an) browser = "Android‰Ø¿¿∆˜";
-            //    if(info.ip) browser = "iPhone‰Ø¿¿∆˜";
-            //};
             
             function getBrowserVersion() {  
                 var browser = {};  
@@ -513,11 +848,11 @@
                    alert ("Browser does not support HTTP Request")
                    return
                 };
-                var url="submitdata.php";
+                var url="index.php";
                 xmlHttp.onreadystatechange=stateChanged;
                 xmlHttp.open("POST",url,true);
                 xmlHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-                xmlHttp.send("browser="+ browser +"&uastring="+ua+"&cssresult="+cssresult+"&sid="+Math.random());
+                xmlHttp.send("browser="+ browser +"&uastring="+ ua +"&"+ cssresult +"&sid="+ Math.random());
             }
             
             function stateChanged() { 
