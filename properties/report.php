ae4e7e648f2cc2f44e84e95c9597f7f93434fa24
王浩 <wb-wanghao@taobao.com>
2012-03-14 11:37:52 +0800

    首页属性数量修改

--- properties/report.php
+++ properties/report.php
@@ -39,8 +39,8 @@
             <h1>CSS Properties Test Result</h1>
             <div id="result" class="result">
             <?php
-                //$con = mysql_connect("127.0.0.1","root","wanghao");
-                $con = mysql_connect("localhost","csstest","csstest");
+                $con = mysql_connect("127.0.0.1","root","wanghao");
+                //$con = mysql_connect("localhost","csstest","csstest");
             
                 if (!$con) {
                     die('Could not connect: ' . mysql_error());
