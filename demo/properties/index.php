310df4876a837d9646e84f1e7263500d0f710089
王浩 <wb-wanghao@taobao.com>
2012-03-22 11:24:11 +0800

    修改demo页宽度

--- demo/properties/index.php
+++ demo/properties/index.php
@@ -11,36 +11,38 @@
 <body>
     <?php include "../../uxcommon/header.php" ?>
     <div class="page_content">
-        <div class="header">
-            <h1>CSS Properties Demo</h1>
-            <a href="../../" title="" class="return">返回首页</a>
-        </div>
-        <div class="box">
-            <h2>弹性盒模型</h2>
-            <ul>
-                <li><a href="box-align&box-orient.html">box-align & box-orient</a></li>
-                <li><a href="box-direction.html">box-direction</a></li>
-                <li><a href="box-flex.html">box-flex</a></li>
-                <li><a href="box-flex-group.html">box-flex-group</a></li>
-                <li><a href="box-lines.html">box-lines</a></li>
-                <li><a href="box-ordinal-group.html">box-ordinal-group</a></li>
-                <li><a href="box-pack&box-orient.html">box-pack & box-orient</a></li>
-            </ul>
-        </div>
-        <div class="box">
-            <h2>多栏</h2>
-            <ul>
-                <li><a href="columns.html">columns</a></li>
-                <li><a href="column-width&column-count.html">column-width & column-count</a></li>
-                <li><a href="column-gap.html">column-gap</a></li>
-                <li><a href="column-rule.html">column-rule</a></li>
-                <li><a href="column-rule-width&column-rule-style&column-rule-color.html">column-rule-width & column-rule-style & column-rule-color</a></li>
-                <li><a href="column-span.html">column-span</a></li>
-                <li><a href="column-fill.html">column-fill</a></li>
-                <li><a href="column-break-before.html">column-break-before</a></li>
-                <li><a href="column-break-after.html">column-break-after</a></li>
-                <li><a href="column-break-inside.html">column-break-inside</a></li>
-            </ul>
+        <div class="wrap">
+            <div class="header">
+                <h1>CSS Properties Demo</h1>
+                <a href="../../" title="" class="return">返回首页</a>
+            </div>
+            <div class="box">
+                <h2>弹性盒模型</h2>
+                <ul>
+                    <li><a href="box-align&box-orient.html">box-align & box-orient</a></li>
+                    <li><a href="box-direction.html">box-direction</a></li>
+                    <li><a href="box-flex.html">box-flex</a></li>
+                    <li><a href="box-flex-group.html">box-flex-group</a></li>
+                    <li><a href="box-lines.html">box-lines</a></li>
+                    <li><a href="box-ordinal-group.html">box-ordinal-group</a></li>
+                    <li><a href="box-pack&box-orient.html">box-pack & box-orient</a></li>
+                </ul>
+            </div>
+            <div class="box">
+                <h2>多栏</h2>
+                <ul>
+                    <li><a href="columns.html">columns</a></li>
+                    <li><a href="column-width&column-count.html">column-width & column-count</a></li>
+                    <li><a href="column-gap.html">column-gap</a></li>
+                    <li><a href="column-rule.html">column-rule</a></li>
+                    <li><a href="column-rule-width&column-rule-style&column-rule-color.html">column-rule-width & column-rule-style & column-rule-color</a></li>
+                    <li><a href="column-span.html">column-span</a></li>
+                    <li><a href="column-fill.html">column-fill</a></li>
+                    <li><a href="column-break-before.html">column-break-before</a></li>
+                    <li><a href="column-break-after.html">column-break-after</a></li>
+                    <li><a href="column-break-inside.html">column-break-inside</a></li>
+                </ul>
+            </div>
         </div>
     </div>
     <?php include "../../uxcommon/footer.php" ?>
